cce45d7020956e853efc59c5e3139d095dc8f189
Kia Choi <sgchoi529@email.arizona.edu>
2017-11-17 15:25:43 -0700

    Trying to link Signup and Login

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import SignUp from './SignUp';
+import Login from './Login';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<SignUp />, document.getElementById('root'));
+ReactDOM.render(<Login />, document.getElementById('root'));
 registerServiceWorker();
